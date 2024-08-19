78914494abd326bbd61f45df6ec016c66c827d9f
sAuRaBhMishRa222 <136999214+sAuRaBhMishRa222@users.noreply.github.com>
2024-08-19 12:46:24 +0530

    Update degen.sol

--- degen.sol
+++ degen.sol
@@ -18,7 +18,34 @@ contract DegenToken is ERC20 {
     }
 
     // Event to log token redemptions
-    event Redeemed(address indexed account, uint256 amount);
+    event Redeemed(address indexed account, string itemName, uint256 amount);
+
+    // Mapping to store the required token amount for each item
+    mapping(string => uint256) public itemCosts;
+
+    // Mapping to store redeemed items for each address
+    mapping(address => string[]) public redeemedItems;
+
+    // Function to set the cost of an item in DegenTokens
+    function setItemCost(string memory itemName, uint256 amount) public onlyOwner {
+        itemCosts[itemName] = amount;
+    }
+
+    // Function to redeem tokens for in-game items
+    function redeemToken(string memory itemName) public {
+        uint256 cost = itemCosts[itemName];
+        require(cost > 0, "Item does not exist");
+        require(balanceOf(msg.sender) >= cost, "Insufficient DegenToken balance to redeem this item");
+
+        // Burn the required tokens
+        _burn(msg.sender, cost);
+
+        // Record the redeemed item
+        redeemedItems[msg.sender].push(itemName);
+
+        // Emit the redemption event
+        emit Redeemed(msg.sender, itemName, cost);
+    }
 
     // Function to mint new tokens
     function mint(address to, uint256 amount) public onlyOwner {
@@ -30,22 +57,14 @@ contract DegenToken is ERC20 {
         _burn(msg.sender, amount);
     }
 
-    // Function to redeem tokens for in-game items
-    function redeemToken(uint256 amount) public {
-        _burn(msg.sender, amount);
-        emit Redeemed(msg.sender, amount);
-    }
-
-    //Function to transfer tokens
+    // Function to transfer tokens
     function transfer(address recipient, uint256 amount) public override returns (bool) {
         _transfer(_msgSender(), recipient, amount);
         return true;
     }
-
-
-    //Function to check the total available tokens
-    function balanceToken(address account) public view returns (uint256) {
-    return super.balanceOf(account);
+    
+    // Function to get redeemed items for a specific address
+    function getRedeemedItems(address account) public view returns (string[] memory) {
+        return redeemedItems[account];
     }
-
 }
